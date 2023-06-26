b97aca7a95aad7c3de149ebd295f43dec4843276
Даниил <daniil@MacBook-Pro-konstantin-2.local>
2023-06-26 13:13:12 +0300

    build DOM

---
+++ html/src/index.js
@@ -0,0 +1,14 @@
+import parser from "./parser.js";
+
+const after = document.getElementById('after');
+const before = document.getElementById('before');
+const button = document.getElementById('button');
+const output = document.getElementById('output');
+
+const alert = () => {
+    const out = parser(before.value, after.value);
+};
+
+button.addEventListener('click', alert);
+
+
---
+++ src/parser.js
@@ -0,0 +1,3 @@
+export default (before, after) => {
+    console.log(before, after);
+};
